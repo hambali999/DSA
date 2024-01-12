507239dc32fb681bfe47fcc9d6fc0f2598348b17
Hambali Nur <chorongbali@gmail.com>
2024-01-12 15:40:17 +0800

    updates

---
+++ CodeWars/7kyu/Mumbling.py
@@ -0,0 +1,33 @@
+# This time no story, no theory. 
+# The examples below show you how to write function accum:
+
+# Examples:
+# accum("abcd") -> "A-Bb-Ccc-Dddd"
+# accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+# accum("cwAt") -> "C-Ww-Aaa-Tttt"
+# The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+
+def accum(s):
+    answer = ""
+    for i in range(0, len(s)):
+        joinword = ""
+        word = s[i]
+        counter = i+1
+        print(i+1, s[i])
+        # joinword = i+1 * s[i]
+        joinword = word*counter
+        print(joinword.capitalize())
+        print("===")
+
+        if counter == len(s):
+            answer += joinword.capitalize()
+        else:
+            answer += joinword.capitalize()+"-"
+
+    print(answer)
+        
+
+accum("RqaEzty")
+
+
